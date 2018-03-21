f5136a15d40eb8aeba9c7cadeb5b7d81b8dd8fa9
DogsRulesAllTime <kuznetsoffar@ya.ru>
2018-03-21 22:33:21 +0300

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -5,15 +5,27 @@
 #include <vector>
 #include <map>
 using namespace std;
+//sozdanie sobstvennogo tipa dannih
+struct human{
+	string name;
+	int age;
+	string gender;
+	char fav_char;
+};
 
 int main() {
+	vector <human>  test;
+	test.push_back({" roman",17,"male",'a'});//probel daet otstyp
+	cout << test[0].age<<endl <<test[0].name<<endl<<"dfsdfdsfsdf\nsdfdsfsdfsdf"<<endl; // \n delaet perenos stroki
+
+
 	int n = 8; //celoe 4islo
 	double dub = 1.9; //drobnoe 4islo
 	char symbol = 'q'; //simvol
 	string stroka = "slovo"; //stroka
 	cout << stroka<< endl;
 
-	vector<int> kek = {1,5,7,8,9}; //nabor dannih
+	vector<int> kek = {1,5,7,8,9}; //nabor dannih odnogo tipa
 	cout << kek.size()<<endl;
 	cout << kek[3];
 
